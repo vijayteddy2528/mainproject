e4d55e4bf313fec880fda3ad7ab05b9f12bcd885
vijay <vijaykumar282004@gmail.com>
2025-08-25 15:19:09 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Product-comparison-MP",
+  base:"/main_project/",
   plugins: [react()],
 })
